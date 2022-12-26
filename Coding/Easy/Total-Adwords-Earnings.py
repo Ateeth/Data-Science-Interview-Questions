2f747d3eaf05916f41922161a51a557f6468d335
Ateeth Arun <58625369+Ateeth@users.noreply.github.com>
2022-12-26 11:11:08 +0300

    Update Total-Adwords-Earnings.py

--- Coding/Easy/Total-Adwords-Earnings.py
+++ Coding/Easy/Total-Adwords-Earnings.py
@@ -1,12 +1,13 @@
 #Question_Link : https://platform.stratascratch.com/coding/10164-total-adwords-earnings?tabname=question&code_type=2
 
 # Import your libraries
-# Import your libraries
 import pandas as pd
 
 google_key_details = google_adwords_earnings[['business_type' , 'adwords_earnings']]
 
-google_key_details.groupby('business_type').sum().reset_index()
+business_earnings = google_key_details.groupby('business_type').sum().reset_index()
+
+business_earnings
 
 
 
