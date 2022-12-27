f0694b7e7e661cd909813fcf44077453cba636f1
Ateeth Arun <58625369+Ateeth@users.noreply.github.com>
2022-12-27 09:49:57 +0300

    Create Duplicate-Emails-9895.py

---
+++ Coding/Easy/Duplicate-Emails-9895.py
@@ -0,0 +1,5 @@
+# Question-Link : https://platform.stratascratch.com/coding/9895-duplicate-emails?tabname=question
+
+# Import your libraries
+import pandas as pd
+result = employee[employee.duplicated('email' , keep = False)]
